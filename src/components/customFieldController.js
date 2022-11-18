85e40629319ea8c9447a4cf651f4803e9f6ba717
KaminiPaliwal05 <kaminip.codetown@gmail.com>
2022-11-18 15:29:16 +0530

    updated

--- src/components/customFieldController.js
+++ src/components/customFieldController.js
@@ -1,23 +1,22 @@
 const customField = require("../packages/addCustomField");
 
 const UsersController = {
- 
-    async customFieldM(req, res) {
+
+  async customFieldM(req, res) {
     try {
-      let data =  await customField.addCustomField({
-          label_name:  req.body.label_name,
-          placeholder: req.body.placeholder,
-          contactid : req.body.contactid
-          });
-        console.log("data",data);
-          res.status(200).send({
-                    message: "Custom field updated to Contact",
-                    data:data
-                  });
+      let data = await customField.addCustomField({
+        label_name: req.body.label_name,
+        placeholder: req.body.placeholder,
+        contactid: req.body.contactid
+      });
+      res.status(200).send({
+        message: "Custom field updated to Contact",
+        data: data
+      });
     } catch (err) {
-        res.status(422).send(err)
+      res.status(422).send(err)
     }
-    }
+  }
 }
 
 module.exports = UsersController;
